958b01f4cd130dd5d018020c577414de05b30ccc
David McFarland <corngood@gmail.com>
2024-01-11 16:33:04 +0000

    codesign: Add support for --remove-signature

--- codesign.cpp
+++ codesign.cpp
@@ -5,9 +5,10 @@ int main(int argc, char **argv) {
     CLI::App app{"codesign"};
 
     std::string identity, identifier, entitlements;
-    bool force = false;
+    bool remove = false, force = false;
     std::vector<std::string> files;
-    app.add_option("-s,--sign", identity, "Code signing identity")->required();
+    auto sign = app.add_option("-s,--sign", identity, "Code signing identity");
+    app.add_flag("--remove-signature", remove, "Remove existing signature");
     app.add_option("-i,--identifier", identifier, "File identifier");
     app.add_flag("-f,--force", force, "Replace any existing signatures");
     app.add_option("--entitlements", entitlements, "Entitlements plist");
@@ -15,19 +16,25 @@ int main(int argc, char **argv) {
 
     CLI11_PARSE(app, argc, argv);
 
-    if (identity != std::string{"-"}) {
-        throw std::runtime_error{
-                std::string{"Only ad-hoc identities supported, requested: '"} + identity + "'"};
-    }
+    if (remove) {
+        for (const auto &f : files) {
+            SigTool::Commands::removeSignature(f);
+        }
+    } else {
+        if (identity != std::string{"-"}) {
+            throw std::runtime_error{
+                    std::string{"Only ad-hoc identities supported, requested: '"} + identity + "'"};
+        }
 
-    SigTool::Commands::CodesignOptions options{
+        SigTool::Commands::CodesignOptions options{
             .identifier = identifier,
             .entitlements = entitlements,
             .force = force,
-    };
+        };
 
-    for (const auto &f : files) {
-	SigTool::Commands::codesign(options, f);
+        for (const auto &f : files) {
+            SigTool::Commands::codesign(options, f);
+        }
     }
 
     return 0;
